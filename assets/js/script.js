28b45a1cd97fa5864e3e60ed0e39a7cc0cd06b39
Bugs Bunny <Cristian.e.cuevas707@gmail.com>
2021-06-23 22:37:00 -0400

    Added passwordGenerator function

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,12 +5,26 @@ var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var lowerCasedCharacters = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", ];
 // Array of uppercase characters to be included in password
 var upperCasedCharacters = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", ];
-
+// New array containing all the arrays before
+var possibleCharacters = specialCharacters.concat(numericCharacters, lowerCasedCharacters, upperCasedCharacters);
 
 function generatePassword() {
-  var passwordLength = prompt("Enter length of password");
-  console.log(passwordLength);
-
+var possibleCharacters = [];
+var passwordLength = parseInt(prompt("Enter length of password"));
+    // If the user enters a non-integer
+  if (isNaN(passwordLength)) {
+    alert("You must enter a number!");
+    return;
+    // if the user enters a number under 8
+  } else if (passwordLength <= 8) {
+    alert("You must enter a number greater than 8!");
+    return;
+    // if the user enters a number over 128
+  } else if (passwordLength >= 128) {
+    alert("You must enter a number less than 128!");
+    return;
+  }
+  
   var containsSpecialCharacters = confirm("Click OK to confirm special characters in your password");
   console.log(containsSpecialCharacters);
 
